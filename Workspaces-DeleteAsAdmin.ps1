019b56a9d85c9f5ea95f4b88190655521241a5b5
Rui Romano <ruiromano@microsoft.com>
2022-01-31 17:37:13 +0000

    changes

---
+++ Workspaces-DeleteAsAdmin.ps1
@@ -0,0 +1,24 @@
+﻿#Requires -Modules @{ ModuleName="MicrosoftPowerBIMgmt"; ModuleVersion="1.2.1026" }
+
+# This script requires you to authenticate with a Power BI Admin account
+
+param(          
+    $searchPattern = "*PBIDevOps*"    
+)
+
+$ErrorActionPreference = "Stop"
+$VerbosePreference = "SilentlyContinue"
+
+Connect-PowerBIServiceAccount
+
+$workspaces  = Get-PowerBIWorkspace -Scope Organization -All
+
+$filteredWorkspaces = @($workspaces |? { $_.Name -like $searchPattern})
+
+if ($filteredWorkspaces.Count -eq 0)
+{
+    Write-Host "No workspace found where name = '$searchPattern'"
+}
+else {
+    # TODO
+}
