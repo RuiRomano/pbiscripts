6a21556c84b88d14b583aeff95228e4ecb9c8b60
Rui Romano <ruiromano@microsoft.com>
2022-02-23 16:25:07 +0000

    Apps Lists & Users
    Get shared links with whole org

---
+++ Apps - ListApps.ps1
@@ -0,0 +1,24 @@
+﻿#Requires -Modules @{ ModuleName="MicrosoftPowerBIMgmt"; ModuleVersion="1.2.1026" }
+
+cls
+
+# Get the authentication token using ADAL library (OAuth)
+
+Connect-PowerBIServiceAccount
+
+# Artifacts
+
+$result = Invoke-PowerBIRestMethod -method Get -url "admin/apps?`$top=1000" | ConvertFrom-Json | Select -ExpandProperty value
+
+$result | Format-Table
+
+Write-Host "App Users"
+
+foreach ($app in $result)
+{
+    Write-Host "App '$($app.id)' users:"
+
+    $users = Invoke-PowerBIRestMethod -method Get -url "admin/apps/$($app.id)/users" | ConvertFrom-Json | Select -ExpandProperty value
+
+    $users | Format-Table
+}
---
+++ Security - GetLinksSharedToWholeOrganization.ps1
@@ -0,0 +1,27 @@
+﻿#Requires -Modules @{ ModuleName="MicrosoftPowerBIMgmt"; ModuleVersion="1.2.1026" }
+
+cls
+
+# Get the authentication token using ADAL library (OAuth)
+
+Connect-PowerBIServiceAccount
+
+# Artifacts
+
+$result = Invoke-PowerBIRestMethod -method Get -url "admin/widelySharedArtifacts/linksSharedToWholeOrganization" | ConvertFrom-Json
+
+$artifactAccessEntities = @()
+
+$artifactAccessEntities += @($result.ArtifactAccessEntities)
+
+while($result.continuationToken -ne $null)
+{          
+    $result = Invoke-PowerBIRestMethod -Url $result.continuationUri -method Get | ConvertFrom-Json
+                                
+    if ($result.ArtifactAccessEntities)
+    {
+        $artifactAccessEntities += @($result.ArtifactAccessEntities)
+    }
+}
+
+$artifactAccessEntities | Format-Table
