9cd1b72e60ca7eb46f994f089f8ed20d9bdfd115
Rui Romano <ruiromano@microsoft.com>
2022-06-23 08:14:54 -0700

    x

---
+++ Workspace - AddUserAsAdmin.ps1
@@ -0,0 +1,22 @@
+﻿#Requires -Modules @{ ModuleName="MicrosoftPowerBIMgmt"; ModuleVersion="1.2.1026" }
+
+cls
+
+# Object Id from Azure AD
+$workspaceId = "f5e73633-29c1-432f-ad5f-3ef5863dcaec"
+
+#https://docs.microsoft.com/en-us/rest/api/power-bi/groups/update-group-user#groupuseraccessright
+$principalObj = @{
+
+    identifier = "2eab306a-bbd8-4f14-8793-eea2d1589585";
+    groupUserAccessRight = "Member";    
+    principalType = "Group"
+}
+
+# Get the authentication token using ADAL library (OAuth)
+
+Connect-PowerBIServiceAccount
+
+$body = $principalObj | ConvertTo-Json
+
+Invoke-PowerBIRestMethod -Url "groups/$workspaceId/users" -Method Post -Body $body
